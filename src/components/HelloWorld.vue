54e86ab7c70a27c4c46dbdf9fd8a6888ab14b217
tecae <tecae_@outlook.com>
2019-05-06 14:36:53 +0300

    edit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,484 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>
+      Joy of T
+      <span>E</span>X
+    </h1>
+    <img src="/favicon.ico" alt srcset>
+    <div>
+      <h1>Асуанская плотина</h1>
+      <p>
+        В 60-х годах XX века во время строительства Асуанской плотины в Египте, под
+        угрозой затопления оказались 24 памятника истории и культуры. Чтобы спасти
+        археологические ценности, строителями плотины была предпринята операция по переносу
+        памятников в безопасное место. Решение этой задачи заняло три года и потребовало
+        усилий строителей и инженеров множества стран, поскольку многие памятники весили
+        несколько сотен тонн. Их перемещение оказалось настолько сложной технической задачей,
+        что некоторые сооружения пришлось разрезать на части. Ни в одной стране мира на тот
+        момент не было техники, которая смогла бы справиться с весом мегалитов.
+      </p>
+      <p>
+        После благополучного спасения археологических ценностей, инженеры задавались одним
+        вопросом: каким образом египтяне, обладая только примитивными инструментами, смогли
+        изготовить и переместить за сотни километров многотонные конструкции?
+      </p>
+    </div>
+    <div>
+      <p>Характеристическое уравнение поверхности второй степени</p>
+      <p>
+        <big>
+          &lambda;
+          <sup>
+            <i>3</i>
+          </sup> -
+          I
+          <sub>
+            <i>1</i>
+          </sub>&lambda;
+          <sup>
+            <i>2</i>
+          </sup> +
+          I
+          <sub>
+            <i>2</i>
+          </sub>&lambda; - I
+          <sub>
+            <i>3</i>
+          </sub> = 0
+        </big>
+      </p>
+    </div>
+    <div>
+      <ul>
+        <li>
+          Пункт 11
+          <ul>
+            <li>пункт 1</li>
+            <li>пункт 2</li>
+            <li>пункт 3</li>
+            <li>пункт 4</li>
+          </ul>
+        </li>
+        <li>
+          Пункт 22
+          <ul>
+            <li>пункт 1</li>
+            <li>пункт 2</li>
+            <li>пункт 3</li>
+            <li>пункт 4</li>
+          </ul>
+        </li>
+        <li>
+          Пункт 33
+          <ul>
+            <li>пункт 1</li>
+            <li>пункт 2</li>
+            <li>пункт 3</li>
+            <li>пункт 4</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="impac">
+      <span>Острий перец</span>
+    </div>
+    <div class="ex-1">
+      <h1>Заголовок 1</h1>
+      <h2>Заголовок 2</h2>
+      <h3>Заголовок 3</h3>
+      <h4>Заголовок 4</h4>
+      <p>
+        Переход по прямой ссылке
+        <a href="http://">прямая ссылка</a>
+      </p>
+    </div>
+    <div class="ex-2">
+      f(x)= a
+      <sub>0</sub>+a
+      <sub>1</sub>x + ... + a
+      <sub>n-1</sub> x
+      <sup>n-1</sup> + a
+      <sub>n</sub> x
+      <sup>n</sup>
+    </div>
+
+    <div class="ex-3">
+      <h1>Ключевые слова</h1>
+      <ol type="I">
+        <li>
+          <b>A</b>
+          <ul type="disc">
+            <li>
+              <a href="#">abstract</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <b>B</b>
+          <ul type="disc">
+            <li>
+              <a href="#">boolean</a>
+            </li>
+            <li>
+              <a href="#">break</a>
+            </li>
+          </ul>
+        </li>
+      </ol>
+    </div>
+
+    <div>
+      <table widht="100%" border="1" cellpadding="4">
+        <caption>Добыча драгоценных камней</caption>
+        <tr>
+          <td></td>
+          <th>2010</th>
+          <th>2012</th>
+          <th>2014</th>
+        </tr>
+        <tr>
+          <td>Сапфир</td>
+          <td rowspan="2">1483</td>
+          <td>4532</td>
+          <td>6743</td>
+        </tr>
+        <tr>
+          <td>Рубин</td>
+          <td>3835</td>
+          <td>7482</td>
+        </tr>
+        <tr>
+          <td>Алиаз</td>
+          <td>683</td>
+          <td colspan="2" align="center">238</td>
+        </tr>
+      </table>
+    </div>
+
+    <div class="ex-4">
+      <h1>Вот такой чай</h1>
+      <p>
+        История о том, как один человек хотел попить чайку, но по ошибке
+        вместо воды попытался налить в чайник бензин, и что из этого получилось.
+      </p>
+    </div>
+
+    <div>
+      <p>
+        Введите число от 1 до 10:
+        <input
+          type="number"
+          name="num"
+          style="width:33px"
+          value="1"
+          id="num"
+          min="1"
+          max="10"
+        >
+      </p>
+    </div>
+
+    <div>
+      <form>
+        <p>
+          <input type="search" name="q" placeholder="Поиск по сайту">
+          <input type="submit" value="Найти">
+        </p>
+      </form>
+    </div>
+
+    <div>
+      <p>
+        Юность, верность, красота,
+        <br>Прелесть сердца, чистота
+        <br>Здесь ледат, сомкнув уста.
+        <br>
+      </p>
+      <p>
+        Феникс умен, и она
+        <br>Отошла, ему верна,
+        <br>В царство вечности и сна
+        <br>
+      </p>
+      <p>
+        Не бесплоден был, о нет,
+        <br>Брак бездетный столько лет,-
+        <br>То невинности обет.
+        <br>
+      </p>
+      <p>
+        Если верность иль - увы!-
+        <br>Красоту найдете вы -
+        <br>То обман, они мертвы.
+        <br>Ты, кто верен и любим,
+        <br>Помолись на благо им
+        <br>Перед камнем гробовым.
+        <br>
+      </p>
+    </div>
+
+    <div>
+      <p style="font-size:1.0em">
+        <a href style="color:blue">Ссылка 1</a>|
+        <a href style="color:chartreuse">Ссылка 2</a>|
+        <a href style="color:red">Ссылка 3</a>
+      </p>
+    </div>
+
+    <div class="img-border">
+      <p>
+        -moz-border-image: url(/border.png) 30 round round;
+        <br>-webkit-border-image: url(/border.png) 30 round round;
+        <br>-o-border-image: url(/border.png) 30 round round;
+        <br>border-image: url(/border.png) 30 round round;
+        <br>
+      </p>
+    </div>
+
+    <div>
+      <p>
+        Горизонтальный отступ от картинки до текста управляется параметром hspace, он добавляет пустое пространство одновременно с лева и справа от изображения.
+        <ins>Поэтому</ins> рисунок от края окна
+        будет отстоять на значение, которое указано этим атрибутом по горизонтали
+        <del>выравнивается не строго по краю окна браузера, а отстоит от него на некотором расстоянии, которое равно значени. hspace.</del>
+      </p>
+      <p>
+        В качестве фона можно указать любое подходящее для этого изображение в формате
+        GIF, JPEG или PNG. Фон не должен отвлекать внимание от текста, при этом должен хорошо сочетатся.
+        <del>Правильно подобраный фон не отвлекает внимание от текста, хорошо сочетается</del> с цветовой гаммой
+        <ins>веб-</ins>страницы, при этом сам файл желательно должен быть небольшим по объему,
+        что бы быстро
+        <del>загружатся</del>
+        <ins>загружался по сети</ins>.
+      </p>
+    </div>
+    <div class="ex-5">
+      <h1>Средствами индивидуальной защиты оператора является:</h1>
+      <p>
+        <a href="#">белый х/б халат темного цвета</a> с антистатической пропиткой
+      </p>
+      <p>
+        <a href="#">экранный защитный фильтр</a> класса "полной защиты";
+      </p>
+      <p>
+        <a href="#">специальные спектральные очки</a>
+      </p>
+    </div>
+    <div>
+      <table width="600" align="center" cellpadding="4" cellspacing="0" border="1">
+        <tr>
+          <td width="150" valign="top">Левая колонка</td>
+          <td valign="top">Правая колонка</td>
+        </tr>
+      </table>
+    </div>
+
+    <div>
+      <h1>Евгений Евтушенко</h1>
+      <h3>-1955-</h3>
+      <p>
+        Бывало, спит у ног собака,
+        <br>костер занявшийся гудит,
+        <br>и женщина из полумрака
+        <br>глазами зыбкими глядит.
+        <br>
+      </p>
+      <p>
+        Потом под пыхтою приляжет
+        <br>на куртку рыжую мою
+        <br>и мне,
+        <br>задумчивая,
+        <br>скажет:
+        <br>А ну-ка, спой!.. - и я пою.
+        <br>
+      </p>
+    </div>
+
+    <div class="ex-6">
+      <div class="japan"></div>
+    </div>
+
+    <div class="ex-7">
+      <h1>Структура файлов</h1>
+      <p>
+        С позиции разработчика, сайт условно можно подразделить на два уровня - логический и физический.
+        На логическом уровне сайта представляет собой совокупность веб-страницы, обедененных между собой
+        единым дизайном, стилем и ссылками. При этом на физическом уровне сайт является и набором файлов разного
+        типа - в состав могут входить программы, документы, изображения и многое другое. Продумывание и создание
+        удобной файловой структуры помогает не только разработчику оптимизировать его работу, но и посетителю
+        лучьше понять строение сайта.
+      </p>
+    </div>
+
+    <div class="ex-8">
+      <span></span>
+      <span class="ch"></span>
+      <span></span>
+      <span class="ch"></span>
+      <span></span>
+    </div>
+    <div>
+      <table class="table-imp" width="300" border="1">
+        <tr>
+          <td>Гиппопотомонстросескиппедалофобия</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.table-imp{
+  width: 300px !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.ex-8 > span {
+  width: 70px;
+  height: 70px;
   display: inline-block;
-  margin: 0 10px;
+  margin-right: 5px;
+  background-color: #000;
 }
-a {
-  color: #42b983;
+
+.ex-8 > span:hover {
+  background-color: chocolate;
+}
+.ex-8 > .ch {
+  background: rgb(40, 166, 250);
+}
+.ex-7 {
+  background-color: #008000;
+  color: #e7f2d7;
+}
+
+.ex-6 .japan {
+  width: 300px;
+  height: 200px;
+  border: 2px solid #ccc;
+  background: radial-gradient(circle, #bc002d 58px, #fff 60px);
+}
+
+.ex-5 > p > a:visited {
+  font-style: oblique;
+}
+
+.img-border {
+  border: 30px solid #40c4c8;
+  padding: 20px;
+  -moz-border-image: url(/border.png) 30 round round;
+  -webkit-border-image: url(/border.png) 30 round round;
+  -o-border-image: url(/border.png) 30 round round;
+  border-image: url(/border.png) 30 round round;
+}
+
+input[type="search"] {
+  color: #dc0000;
+}
+
+h1 {
+  font-family: "Times New Roman", Times, serif;
+  letter-spacing: -0.1em;
+  font-size: 3em;
+}
+h2 {
+  color: chartreuse;
+  color: chartreuse;
+}
+h3 {
+  color: chocolate;
+}
+p {
+  font: 14px/1.4 Arial, sans-serif;
+}
+img {
+  box-shadow: inset 0 0 10px rgb(78, 78, 78);
+}
+h1 span {
+  vertical-align: -0.2em;
+}
+.impac {
+  font: 60px Impact, sans-serif;
+  background-color: brown;
+  color: aliceblue;
+  font-size: 3em;
+}
+.impac span {
+  background-color: brown;
+  bottom: -13px; /* Сдвигаем текст вниз */
+}
+.ex-1 {
+  background-color: rgb(236, 236, 243);
+}
+.ex-1 h1 {
+  color: aqua;
+}
+.ex-1 > h1 {
+  font-size: 1.8em;
+}
+.ex-1 > p a {
+  color: aqua;
+}
+.ex-2 {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  font-style: italic;
+  font-size: 1.3em;
+}
+.ex-2 sub .ex-2 sup {
+  font-style: normal;
+  font-size: 0.6em;
+}
+.ex-2 sub {
+  color: blue;
+}
+.ex-2 sup {
+  color: red;
+}
+.ex-4 > p {
+  background: #cad8d0;
+  padding: 10px;
+  margin: 0px;
+}
+.ex-4 > h1 {
+  margin: 0px;
+  background: #569099;
+  padding: 10px;
+  color: #fff;
+}
+
+a:link {
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+  font-size: 11px;
+}
+a:link a:visited a:hover {
+  text-decoration: none;
+  color: #3a681a;
+}
+a:hover {
+  color: #5ca22e;
+}
+a:active {
+  color: #dc0000;
+}
+.pole .pole2 a:link {
+  height: 11px;
+}
+.pole .pole2 {
+  border: 1px solid #008000;
+  background-color: #e7f2d7;
+  height: 17px;
+}
+.pole {
+  width: 95px;
+  font-size: 11px;
 }
 </style>
